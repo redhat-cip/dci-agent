f3c03849c7a620e6cf6cd1f4b898e83fb4b8c731
Gonéri Le Bouder <goneri@redhat.com>
2016-08-19 15:29:04 +0200

    RPM: add the spec file and associated scripts
    Change-Id: I1d02d77ee0a7b040599078acafa701485bf9c2bd

---
+++ build_rpm.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -eux
+PROJ_NAME=dci-agent
+DATE=$(date +%Y%m%d%H%M)
+SHA=$(git rev-parse HEAD | cut -c1-8)
+
+# Configure rpmmacros to enable signing packages
+#
+echo '%_signature gpg' >> ~/.rpmmacros
+echo '%_gpg_name Distributed-CI' >> ~/.rpmmacros
+
+# Create the proper filesystem hierarchy to proceed with srpm creatioon
+#
+rm -rf ${HOME}/rpmbuild
+rpmdev-setuptree
+sed -i "s,version='\(.*\)',version='0.0.${DATE}git${SHA}'," setup.py
+python setup.py sdist
+cp -v dist/* ${HOME}/rpmbuild/SOURCES/
+sed "s/VERS/${DATE}git${SHA}/g" ${PROJ_NAME}.spec > ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
+
+rpmbuild -bs ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
+
+# Build the RPMs in a clean chroot environment with mock to detect missing
+# BuildRequires lines.
+for arch in fedora-23-x86_64 epel-7-x86_64; do
+    rpath=$(echo ${arch}|sed s,-,/,g)
+
+    # NOTE(spredzy): Include the dci repo in mock env
+    mkdir -p ${HOME}/.mock development
+    # NOTE(Gonéri): We ignore the last """ from the last line to be able to concat
+    # our stuff
+    head -n -1 /etc/mock/${arch}.cfg > ${HOME}/.mock/${arch}-with-dci-repo.cfg
+    cat <<EOF >> ${HOME}/.mock/${arch}-with-dci-repo.cfg
+[dci-devel]
+name="Distributed CI - Devel - CentOS 7"
+baseurl=http://packages.distributed-ci.io/repos/development/el/7/x86_64/
+gpgcheck=0
+enabled=1
+
+[dci-extras]
+name="Distributed CI - No upstream package - CentOS 7"
+baseurl=http://packages.distributed-ci.io/repos/extras/el/7/x86_64/
+gpgcheck=0
+enabled=1
+"""
+# NOTE(spredzy) Add signing options
+#
+config_opts['plugin_conf']['sign_enable'] = True
+config_opts['plugin_conf']['sign_opts'] = {}
+config_opts['plugin_conf']['sign_opts']['cmd'] = 'rpmsign'
+config_opts['plugin_conf']['sign_opts']['opts'] = '--addsign %(rpms)s'
+EOF
+    mock -r ${HOME}/.mock/${arch}-with-dci-repo.cfg --rebuild --resultdir=development/${rpath} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
+done
