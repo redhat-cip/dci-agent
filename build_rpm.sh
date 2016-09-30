1873efb70a5cdbf8bf1f70309322a9b5fd6e3852
Yanis Guenane <yguenane@redhat.com>
2016-09-30 09:08:12 +0200

    Initial commit
    Change-Id: Icc12a04586ad866d18ca8440783b9d9430a3e8de

--- build_rpm.sh
+++ build_rpm.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 set -eux
-PROJ_NAME=dci-agent
+PROJ_NAME=dciagent
 DATE=$(date +%Y%m%d%H%M)
-SHA=$(git log -1 --pretty=tformat:%h)
-
-# Configure rpmmacros to enable signing packages
-#
-echo '%_signature gpg' >> ~/.rpmmacros
-echo '%_gpg_name Distributed-CI' >> ~/.rpmmacros
+SHA=$(git rev-parse HEAD | cut -c1-8)
 
 # Create the proper filesystem hierarchy to proceed with srpm creatioon
 #
 rm -rf ${HOME}/rpmbuild
+mock --clean
 rpmdev-setuptree
-sed -i "s,version = '\(.*\)',version = '0.0.${DATE}git${SHA}'," dci_agent/__init__.py
-python setup.py sdist
-cp -v dist/* ${HOME}/rpmbuild/SOURCES/
-cp -v dci_agent/systemd/* ${HOME}/rpmbuild/SOURCES/
-sed "s/VERS/${DATE}git${SHA}/g" ${PROJ_NAME}.spec > ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
-
+cp ${PROJ_NAME}.spec ${HOME}/rpmbuild/SPECS/
+git archive HEAD --format=tgz --output=${HOME}/rpmbuild/SOURCES/${PROJ_NAME}-0.0.${DATE}git${SHA}.tgz
+cp  conf/dci_agent.conf ${HOME}/rpmbuild/SOURCES/
+cp  conf/dci_agent.conf.d/* ${HOME}/rpmbuild/SOURCES/
+sed -i "s/VERS/${DATE}git${SHA}/g" ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 rpmbuild -bs ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 
-# Build the RPMs in a clean chroot environment with mock to detect missing
-# BuildRequires lines.
 for arch in fedora-23-x86_64 fedora-24-x86_64 epel-7-x86_64; do
     rpath=$(echo ${arch}|sed s,-,/,g|sed 's,epel,el,')
 
-    # NOTE(spredzy): Include the dci repo in mock env
-    mkdir -p ${HOME}/.mock development
-    # NOTE(Gonéri): We ignore the last """ from the last line to be able to concat
-    # our stuff
-    head -n -1 /etc/mock/${arch}.cfg > ${HOME}/.mock/${arch}-with-dci-repo.cfg
-    cat <<EOF >> ${HOME}/.mock/${arch}-with-dci-repo.cfg
+    mkdir -p ${HOME}/.mock
+    cp /etc/mock/${arch}.cfg ${HOME}/.mock/${arch}-with-extras.cfg
+
+    RPATH='el/7/x86_64'
+    head -n -1 /etc/mock/${arch}.cfg > ${HOME}/.mock/${arch}-with-extras.cfg
+    cat <<EOF >> ${HOME}/.mock/${arch}-with-extras.cfg
 [dci-devel]
 name="Distributed CI - Devel - CentOS 7"
 baseurl=http://packages.distributed-ci.io/repos/development/el/7/x86_64/
@@ -44,12 +37,10 @@ baseurl=http://packages.distributed-ci.io/repos/extras/el/7/x86_64/
 gpgcheck=0
 enabled=1
 """
-# NOTE(spredzy) Add signing options
-#
-config_opts['plugin_conf']['sign_enable'] = True
-config_opts['plugin_conf']['sign_opts'] = {}
-config_opts['plugin_conf']['sign_opts']['cmd'] = 'rpmsign'
-config_opts['plugin_conf']['sign_opts']['opts'] = '--addsign %(rpms)s'
 EOF
-    mock -r ${HOME}/.mock/${arch}-with-dci-repo.cfg --no-clean --rebuild --resultdir=development/${rpath} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
+
+    # Build the RPMs in a clean chroot environment with mock to detect missing
+    # BuildRequires lines.
+    mkdir -p development
+    mock -r ${HOME}/.mock/${arch}-with-extras.cfg rebuild --resultdir=development/${RPATH} ${HOME}/rpmbuild/SRPMS/${PROJ_NAME}*
 done
