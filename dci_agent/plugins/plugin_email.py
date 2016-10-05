40e6a5b1af7a23d76b850b033512af3808561828
Yanis Guenane <yguenane@redhat.com>
2016-10-05 11:24:13 +0200

    import the plugins file for the new agent
    This commit imports the plugins file necessary for the new plugin
    architecture. It doesn't change the way current implementation works as
    they won't be used currently just imported.
    
    Change-Id: Iaa11618bc67ee9f6e51fd57fdede505543ee5dd0

---
+++ dci_agent/plugins/plugin_email.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Plugin to send email notification
+
+  :param string subject: Subject of the mail to send
+  :param string message: Content of the mail body
+  :param string smtp_server: Address of the STMP server to use
+  :param int smtp_port: Port of the SMTP server to bind to
+  :param string smtp_user: User to connect to the SMTP server
+  :param string smtp_password: Password to connect to the SMTP server
+  :param string smtp_recpt: Comma separated list of email receivers
+
+"""
+
+from dciagent.plugins import plugin
+
+
+class Email(plugin.Plugin):
+
+    def __init__(self, conf):
+        super(Email, self).__init__(conf)
+
+    def run(self, state, data=None, context=None):
+        """Send a notification email. """
+
+        subject = self.conf[state]['subject']
+        message = self.conf[state]['message']
+
+        smtp_server = self.conf['smtp_server']
+        smtp_port = self.conf['smtp_port']
+        smtp_user = self.conf['smtp_user']
+        smtp_passwd = self.conf['smtp_password']
+        smtp_recpt = self.conf['smtp_recpt']
+
+        subj = self.format(subject, data, context)
+        msg = self.format(message, data, context)
+
+        ansible_play = """
+- hosts: localhost
+  tasks:
+    - mail: host='%s'
+            port=%s
+            username='%s'
+            password='%s'
+            to='%s'
+            subject='%s'
+            body='%s'
+
+""" % (smtp_server, smtp_port, smtp_user, smtp_passwd, smtp_recpt, subj, msg)
+
+        self.run_playbook(playbook, context)
---
+++ dci_agent/plugins/plugin_irc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Plugin to send irc notification
+
+  :param string message: Message to send to the IRC channel
+  :param string server: IRC server to connect to
+  :param int port: IRC server port to connect to
+  :param string chan: IRC channel to connect to
+  :param string nick: Nick to use when connecting to IRC
+
+"""
+
+from dciagent.plugins import plugin
+
+
+class Irc(plugin.Plugin):
+
+    def __init__(self, conf):
+        super(Irc, self).__init__(conf)
+
+    def run(self, state, data=None, context=None):
+        """Connect to the specified IRC server/channel and post a message. """
+
+        message = self.conf[state]['message']
+
+        server = self.conf['server']
+        port = self.conf['port']
+        chan = self.conf['chan']
+
+        nick = self.conf['nick']
+
+        ansible_play = """
+- hosts: localhost
+  tasks:
+    - irc: channel='%s'
+           server='%s'
+           msg='%s'
+           nick='%s'
+           port='%s'
+""" % (chan, server, self.format(message, data, context), nick, port)
+
+        self.run_playbook(playbook, context)
