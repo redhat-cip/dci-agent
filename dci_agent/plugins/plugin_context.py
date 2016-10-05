40522151b642977489083c51fd69eebe98dae983
Yanis Guenane <yguenane@redhat.com>
2016-10-05 11:24:13 +0200

    Import the plugins file for the new agent
    This commit imports the plugins file necessary for the new plugin
    architecture. It doesn't change the way current implementation works as
    they won't be used currently just imported
    
    Change-Id: Iaa11618bc67ee9f6e51fd57fdede505543ee5dd0

---
+++ dci_agent/plugins/plugin_context.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Plugin to upload file to the Control-Server
+
+  :param array path: Array of path to files to upload
+
+"""
+
+from dci_agent.plugins import plugin
+from dciclient.v1.api import file as dci_file
+
+
+class Context(plugin.Plugin):
+
+    def __init__(self, conf):
+        super(Context, self).__init__(conf)
+
+    def run(self, state, data=None, context=None):
+        """Upload configuration files. """
+
+        if 'path' in self.conf:
+            for f in self.conf['path']:
+                try:
+                    content = open(f, 'r').read()
+                    dci_file.create(context=context, name=f,
+                                    content=content,
+                                    job_id=context.last_job_id)
+                except OSError as e:
+                    raise(e)
+                except IOError as e:
+                    raise(e)
