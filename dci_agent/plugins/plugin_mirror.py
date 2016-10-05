6f87d61e56daea1993136dc78d792a2adb020e16
Yanis Guenane <yguenane@redhat.com>
2016-10-05 11:24:13 +0200

    Import the plugins file for the new agent
    This commit imports the plugins file necessary for the new plugin
    architecture. It doesn't change the way current implementation works as
    they won't be used currently just imported.
    
    Change-Id: Iaa11618bc67ee9f6e51fd57fdede505543ee5dd0

---
+++ dci_agent/plugins/plugin_mirror.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Plugin to synchronize local mirror before running the actual agent.
+"""
+
+from dciagent.plugins import plugin
+
+
+import os
+
+
+class Mirror(plugin.Plugin):
+
+    def __init__(self, conf):
+        super(Mirror, self).__init__(conf)
+
+    def run(self, state, data=None, context=None):
+        """Sync remote repository to local mirror. """
+
+        base_dir = self.conf.get('directory', '/srv/puddles')
+
+        for component in data['components']:
+
+            dest = '%s/%s' % (base_dir, component['data']['path'])
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+
+            repo = component['data']['repo_name']
+            data_path = component['data']['path']
+
+            playbook = """
+- hosts: localhost
+  vars:
+    dci_status: %s
+    dci_comment: 'Synchronizing repositories %s'
+    dci_log_prefix: 'mirror'
+  tasks:
+    - name: Synchronize the puddles repository %s
+      shell: rsync -av --hard-links partner@rhos-mirror.distributed-ci.io:/srv/puddles/%s %s/../
+
+    - name: Create yum repo for %s
+      yum_repository:
+        name: %s
+        description: %s
+        baseurl: file://%s
+        enabled: 1
+        gpgcheck: 0
+        priority: 0
+""" % (state, repo, repo, data_path, dest, repo, repo, repo, dest)  # noqa
+
+            self.run_playbook(playbook, context)
