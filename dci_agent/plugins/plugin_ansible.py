328111a00bf662aaf63ea8394220feaeefea27ef
Yanis Guenane <yguenane@redhat.com>
2016-10-05 11:24:13 +0200

    Import the plugins file for the new agent
    This commit imports the plugins file necessary for the new plugin
    architecture. It doesn't change the way current implementation works as
    they won't be used currently just imported
    
    Change-Id: Iaa11618bc67ee9f6e51fd57fdede505543ee5dd0

---
+++ dci_agent/plugins/plugin_ansible.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Red Hat, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Plugin to run ansible playbook
+
+  :param string playbook: Path to playbook to run
+  :param string template: Path to playbook template to generate
+
+"""
+
+import jinja2
+
+from dciagent.plugins import plugin
+
+
+class Ansible(plugin.Plugin):
+
+    def __init__(self, conf):
+        super(Ansible, self).__init__(conf)
+
+    def generate_ansible_playbook_from_template(self, template_file, data):
+        templateLoader = jinja2.FileSystemLoader(searchpath="/")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        template = templateEnv.get_template(template_file)
+        outputText = template.render(data)
+
+        return outputText
+
+    def run(self, state, data=None, context=None):
+        """Run ansible-playbook on the specified playbook. """
+
+        playbook = None
+        template = None
+
+        if state in self.conf:
+            if 'playbook' in self.conf[state]:
+                playbook = self.conf[state]['playbook']
+            if 'template' in self.conf[state]:
+                template = self.conf[state]['template']
+
+        if playbook is None:
+            playbook = self.conf['playbook']
+        if template is None and template in self.conf:
+            template = self.conf['template']
+
+        if template:
+            open(playbook, 'w').write(
+                self.generate_ansible_playbook_from_template(template, data)
+            )
+
+        kwargs = {}
+        if 'certification_id' in data['remoteci']['data']:
+            kwargs.update({'certification_id':
+                           data['remoteci']['data']['certification_id']})
+
+        self.run_playbook(playbook, context, **kwargs)
