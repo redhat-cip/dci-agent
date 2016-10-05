47bfd9df2b40e6dccfdca023d9f5f4f005d37ec5
Yanis Guenane <yguenane@redhat.com>
2016-10-05 10:15:24 +0200

    Import of the configuration file for the new agent
    This commit imports the configuration file necessary for the new plugin
    architecture. It doesn't change the way current implementation works as
    they won't be used currently just imported.
    
    Change-Id: I117531b543db35322576bce0df77345481e2a2c2

--- build_rpm.sh
+++ build_rpm.sh
@@ -17,6 +17,8 @@ sed -i "s,version = '\(.*\)',version = '0.0.${DATE}git${SHA}'," dci_agent/__init
 python setup.py sdist
 cp -v dist/* ${HOME}/rpmbuild/SOURCES/
 cp -v dci_agent/systemd/* ${HOME}/rpmbuild/SOURCES/
+cp -v conf/dci_agent.yaml ${HOME}/rpmbuild/SOURCES/
+cp -v conf/dci_agent.conf.d/* ${HOME}/rpmbuild/SOURCES/
 sed "s/VERS/${DATE}git${SHA}/g" ${PROJ_NAME}.spec > ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
 
 rpmbuild -bs ${HOME}/rpmbuild/SPECS/${PROJ_NAME}.spec
