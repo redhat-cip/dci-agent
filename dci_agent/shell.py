324b3d4c761f13833ac5b46813cb35ad0a74018c
Yanis Guenane <yguenane@redhat.com>
2016-10-06 11:00:55 +0200

    Avoid namespace collision during import
    The module name is 'dci_agent' the script name is 'dci_agent', this
    result in namespace collision during import.
    
    This make one uses workaround like from . import X because it can not
    import correctly the wished module.
    
    This commit fixes that by chaning the name of the script to shell.py
    and demonstrating it works by importing version.__version__ from the
    shell.py itself.
    
    Change-Id: Icbdb9cf881d36035d3e32efd786a57fdb84d0c0c

--- dci_agent/shell.py
+++ dci_agent/shell.py
@@ -15,8 +15,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from dci_agent import config as conf
-from dci_agent import utils
 from dci_agent import version
 from dciclient.v1.api import context as dci_context
 from dciclient.v1.api import file as dci_file
@@ -30,44 +28,31 @@ from dciclient.v1 import tripleo_helper as dci_tripleo_helper
 import tripleohelper.server
 import tripleohelper.undercloud
 
-import click
+import argparse
 import logging
 import os
 import os.path
 import subprocess
 import sys
 import traceback
+import yaml
+
+
+def load_config(config_path):
+    with open(config_path, 'r') as fd:
+        dci_conf = yaml.load(fd)
+    if 'dci_cs_url' not in dci_conf['auth']:
+        dci_conf['auth']['dci_cs_url'] = 'https://api.distributed-ci.io'
+    if 'key_filename' not in dci_conf:
+        dci_conf['key_filename'] = os.path.expanduser('~/.ssh/id_rsa')
+    if 'repository' not in dci_conf['mirror']:
+        dci_conf['mirror']['repository'] = '/var/www/html'
+    return dci_conf
 
 
 def get_dci_context(**args):
-    """Retrieve a DCI context from the dciclient. """
-
     args['user_agent'] = 'dci-agent-' + version.__version__
-    if 'dci_cs_url' not in args:
-        args['dci_cs_url'] = 'https://api.distributed-ci.io'
-
     return dci_context.build_dci_context(**args)
-
-
-def get_dci_job_data(ctx, **dci):
-    """Retrieve informations about the job to run. """
-
-    topic_id = dci_topic.get(ctx, dci['topic']).json()['topic']['id']
-    remoteci = dci_remoteci.get(ctx, dci['remoteci']).json()
-    remoteci_id = remoteci['remoteci']['id']
-
-    r = dci_job.schedule(ctx, remoteci_id, topic_id=topic_id)
-    if r.status_code == 412:
-        logging.info('Nothing to do')
-        exit(0)
-    elif r.status_code != 201:
-        logging.error('Unexpected code: %d' % r.status_code)
-        logging.error(r.text)
-        exit(1)
-
-    job_full_data = dci_job.get_full_data(ctx, ctx.last_job_id)
-
-    return job_full_data
 
 
 def init_undercloud_host(undercloud_ip, key_filename):
@@ -115,158 +100,110 @@ priority=0
                 path=c['data']['path']))
 
 
-@click.command()
-@click.option('--topic', envvar='DCI_AGENT_TOPIC', required=False,
-              help="Topic the agent apply to.")
-@click.option('--config', envvar='DCI_AGENT_CONFIG', required=False,
-              help="Path to DCI config file.")
-def main(config=None, topic=None):
+def main(argv=None):
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--topic')
+    parser.add_argument('--config', default='/etc/dci/dci_agent.yaml')
+    parser.add_argument('--version', action='version',
+                        version=('dci-agent %s' % version.__version__))
+    args = parser.parse_args(argv)
 
-    dci_conf = conf.load_config(config)
+    dci_conf = load_config(args.config)
     ctx = get_dci_context(**dci_conf['auth'])
+    topic_name = args.topic if args.topic else dci_conf['topic']
+    topic = dci_topic.get(ctx, topic_name).json()['topic']
+    remoteci = dci_remoteci.get(ctx, dci_conf['remoteci']).json()['remoteci']
+    r = dci_job.schedule(ctx, remoteci['id'], topic_id=topic['id'])
+    if r.status_code == 412:
+        logging.info('Nothing to do')
+        exit(0)
+    elif r.status_code != 201:
+        logging.error('Unexpected code: %d' % r.status_code)
+        logging.error(r.text)
+        exit(1)
+    components = dci_job.get_components(
+        ctx, ctx.last_job_id).json()['components']
+    logging.debug(components)
 
-    if topic:
-        topic_name = topic
-    elif 'topic' in dci_conf:
-        topic_name = dci_conf['topic']
-    elif 'dci' in dci_conf:
-        topic_name = dci_conf['dci']['topic']
-
-    if 'remoteci' in dci_conf:
-        remoteci_name = dci_conf['remoteci']
-    elif 'dci' in dci_conf:
-        remoteci_name = dci_conf['dci']['remoteci']
-
-    cnf = {'topic': topic_name, 'remoteci': remoteci_name}
-    job_data = get_dci_job_data(ctx, **cnf)
-
-    logging.debug(job_data['components'])
-
-    # This is the core of the v2 of the agent
-    # It is triggered only if a 'dci' section is present in the
-    # configuration file.
-    if 'dci' in dci_conf:
-        RV = 0
-        states = ['new', 'pre-run', 'running', 'post-run', 'success']
-        for state in states:
-            if state in dci_conf['dci'] and RV == 0:
-                for hook in dci_conf['dci'][state]:
-                    dci_jobstate.create(ctx, state, 'Running %s hook' % hook,
-                                        ctx.last_job_id)
-                    # load the plugin associated to the hook and then run it
-                    plugin_class = utils.load_plugin(hook)
-                    RV = plugin_class(dci_conf[hook]).run(
-                        state, data=job_data, context=ctx,
-                        auth=dci_conf['auth']
-                    )
-                    if RV != 0:
-                        if 'failure' in dci_conf['dci']:
-                            for hook in dci_conf['dci']['failure']:
-                                dci_jobstate.create(ctx, 'failure',
-                                                    'Running %s hook' % hook,
-                                                    ctx.last_job_id)
-                                # load the plugin associated to the hook and
-                                # then run it
-                                plugin_class = utils.load_plugin(hook)
-                                plugin_class(dci_conf[hook]).run(
-                                    'failure', data=job_data, context=ctx,
-                                    auth=dci_conf['auth']
-                                )
-                        else:
-                            dci_jobstate.create(
-                                ctx, 'failure',
-                                'A failure occured during the agent run',
-                                ctx.last_job_id
-                            )
-                        break
-
-        # Handle the push of the last 'success' job state if no plugin
-        # have been configured to take an action on success
-        if RV == 0 and 'success' not in dci_conf['dci']:
-            dci_jobstate.create(ctx, 'success', 'Successfully ran the agent',
-                                ctx.last_job_id)
-
-    # This is the core of the v1 of the agent
-    # This code is run by default except if a 'dci' section is specified in
-    # the configuration file
-    else:
-        try:
-            prepare_local_mirror(ctx,
-                                 dci_conf['mirror']['directory'],
-                                 dci_conf['mirror']['url'],
-                                 job_data['components'])
-            dci_jobstate.create(ctx, 'pre-run', 'director node provisioning',
-                                ctx.last_job_id)
-            for c in dci_conf['hooks']['provisioning']:
-                dci_helper.run_command(ctx, c, shell=True)
-            if 'undercloud_ip_command' in dci_conf:
-                undercloud_ip = subprocess.Popen(
-                    dci_conf['undercloud_ip_command'],
-                    shell=True,
-                    stdout=subprocess.PIPE).stdout.read().rstrip()
-            else:
-                undercloud_ip = dci_conf['undercloud_ip']
-            init_undercloud_host(undercloud_ip,
-                                 dci_conf['key_filename'])
-            dci_jobstate.create(
-                ctx,
-                'running',
-                'undercloud deployment',
-                ctx.last_job_id)
-            for c in dci_conf['hooks']['undercloud']:
-                dci_helper.run_command(ctx, c, shell=True)
-            dci_jobstate.create(ctx, 'running', 'overcloud deployment',
-                                ctx.last_job_id)
-            for c in dci_conf['hooks']['overcloud']:
-                dci_helper.run_command(ctx, c, shell=True)
-            dci_tripleo_helper.run_tests(
-                ctx,
-                undercloud_ip=dci_conf['undercloud_ip'],
-                key_filename=dci_conf['key_filename'],
-                remoteci_id=job_data['remoteci']['id'],
-                stack_name=dci_conf.get('stack_name', 'overcloud'))
-            final_status = 'success'
-            backtrace = ''
-            msg = ''
-        except Exception as e:
-            final_status = 'failure'
-            backtrace = traceback.format_exc()
-            msg = str(e)
-            pass
-
-        # Teardown should happen even in case of failure and should not make
-        # the agent run fail.
-        try:
-            teardown_commands = dci_conf['hooks'].get('teardown')
-            if teardown_commands:
-                dci_jobstate.create(ctx, 'post-run', 'teardown',
-                                    ctx.last_job_id)
-                for c in teardown_commands:
-                    dci_helper.run_command(ctx, c, shell=True)
-        except Exception as e:
-            backtrace_teardown = str(e) + '\n' + traceback.format_exc()
-            logging.error(backtrace_teardown)
-            dci_file.create(
-                ctx,
-                'backtrace_teardown',
-                backtrace_teardown,
-                mime='text/plain',
-                jobstate_id=ctx.last_jobstate_id)
-            pass
-
+    try:
+        prepare_local_mirror(ctx,
+                             dci_conf['mirror']['directory'],
+                             dci_conf['mirror']['url'],
+                             components)
+        dci_jobstate.create(ctx, 'pre-run', 'director node provisioning',
+                            ctx.last_job_id)
+        for c in dci_conf['hooks']['provisioning']:
+            dci_helper.run_command(ctx, c, shell=True)
+        init_undercloud_host(dci_conf['undercloud_ip'],
+                             dci_conf['key_filename'])
+        if 'undercloud_ip_command' in dci_conf:
+            undercloud_ip = subprocess.Popen(
+                dci_conf['undercloud_ip_command'],
+                shell=True,
+                stdout=subprocess.PIPE).stdout.read().rstrip()
+        else:
+            undercloud_ip = dci_conf['undercloud_ip']
+        # init_undercloud_host(undercloud_ip,
+        #                      dci_conf['key_filename'])
         dci_jobstate.create(
             ctx,
-            final_status,
-            msg,
+            'running',
+            'undercloud deployment',
             ctx.last_job_id)
-        logging.info('Final status: ' + final_status)
-        if backtrace:
-            logging.error(backtrace)
-            dci_file.create(
-                ctx,
-                'backtrace',
-                backtrace,
-                mime='text/plain',
-                jobstate_id=ctx.last_jobstate_id)
-        sys.exit(0 if final_status == 'success' else 1)
+        for c in dci_conf['hooks']['undercloud']:
+            dci_helper.run_command(ctx, c, shell=True)
+        dci_jobstate.create(ctx, 'running', 'overcloud deployment',
+                            ctx.last_job_id)
+        for c in dci_conf['hooks']['overcloud']:
+            dci_helper.run_command(ctx, c, shell=True)
+        dci_tripleo_helper.run_tests(
+            ctx,
+            undercloud_ip=undercloud_ip,
+            key_filename=dci_conf['key_filename'],
+            remoteci_id=remoteci['id'],
+            stack_name=dci_conf.get('stack_name', 'overcloud'))
+        final_status = 'success'
+        backtrace = ''
+        msg = ''
+    except Exception as e:
+        final_status = 'failure'
+        backtrace = traceback.format_exc()
+        msg = str(e)
+        pass
+
+    # Teardown should happen even in case of failure and should not make the
+    # agent run fail.
+    try:
+        teardown_commands = dci_conf['hooks'].get('teardown')
+        if teardown_commands:
+            dci_jobstate.create(ctx, 'post-run', 'teardown',
+                                ctx.last_job_id)
+            for c in teardown_commands:
+                dci_helper.run_command(ctx, c, shell=True)
+    except Exception as e:
+        backtrace_teardown = str(e) + '\n' + traceback.format_exc()
+        logging.error(backtrace_teardown)
+        dci_file.create(
+            ctx,
+            'backtrace_teardown',
+            backtrace_teardown,
+            mime='text/plain',
+            jobstate_id=ctx.last_jobstate_id)
+        pass
+
+    dci_jobstate.create(
+        ctx,
+        final_status,
+        msg,
+        ctx.last_job_id)
+    logging.info('Final status: ' + final_status)
+    if backtrace:
+        logging.error(backtrace)
+        dci_file.create(
+            ctx,
+            'backtrace',
+            backtrace,
+            mime='text/plain',
+            jobstate_id=ctx.last_jobstate_id)
+    sys.exit(0 if final_status == 'success' else 1)
