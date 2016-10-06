7a7da4e02ad60b38fe53d6dac9a24545d8f28fba
Yanis Guenane <yguenane@redhat.com>
2016-10-06 11:00:55 +0200

    Avoid namespace collision during import
    The module name is 'dci_agent' the script name is 'dci_agent', this
    result in namespace collision during import.
    
    This make one uses workaround like from . import X because it can not
    import correctly the wished module.
    
    This commit fixes that by chaning the name of the script to shell.py
    and demonstrating it works by importing version.__version__ from the
    shell.py itself.
    
    Change-Id: Icbdb9cf881d36035d3e32efd786a57fdb84d0c0c

--- dci_agent/shell.py
+++ dci_agent/shell.py
@@ -15,7 +15,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from . import version
+from dci_agent import version
 from dciclient.v1.api import context as dci_context
 from dciclient.v1.api import file as dci_file
 from dciclient.v1.api import job as dci_job
@@ -50,7 +50,7 @@ def load_config(config_path):
 
 
 def get_dci_context(**args):
-    args['user_agent'] = 'dci-agent-' + version
+    args['user_agent'] = 'dci-agent-' + version.__version__
     return dci_context.build_dci_context(**args)
 
 
@@ -105,7 +105,7 @@ def main(argv=None):
     parser.add_argument('--topic')
     parser.add_argument('--config', default='/etc/dci/dci_agent.yaml')
     parser.add_argument('--version', action='version',
-                        version=('dci-agent %s' % version))
+                        version=('dci-agent %s' % version.__version__))
     args = parser.parse_args(argv)
 
     dci_conf = load_config(args.config)
