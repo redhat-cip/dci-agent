7998264a4bbbaba76054571f4dc576ce13fb938a
Yanis Guenane <yguenane@redhat.com>
2016-12-01 09:24:29 +0100

    Better parsing of arguments
    Change-Id: I466650abbd3e1ced7660462a68aa6ae98fa29cba

--- dci_agent/shell.py
+++ dci_agent/shell.py
@@ -126,8 +126,17 @@ def main(config=None, topic=None):
     dci_conf = conf.load_config(config)
     ctx = get_dci_context(**dci_conf['auth'])
 
-    topic_name = topic if topic else dci_conf['topic']
-    cnf = {'topic': topic_name, 'remoteci': dci_conf['remoteci']}
+    if 'dci' in dci_conf:
+        topic_name = dci_conf['dci']['topic']
+        remoteci_name = dci_conf['dci']['remoteci']
+    else:
+        remoteci_name = dci_conf['remoteci']
+        if 'topic' in dci_conf:
+            topic_name = dci_conf['topic']
+        else:
+            topic_name = topic
+
+    cnf = {'topic': topic_name, 'remoteci': remoteci_name}
     job_data = get_dci_job_data(ctx, **cnf)
 
     logging.debug(job_data['components'])
@@ -139,7 +148,7 @@ def main(config=None, topic=None):
         RV = 0
         states = ['new', 'pre-run', 'running', 'post-run', 'success']
         for state in states:
-            if state in dci_conf['dci'] and RV != 0:
+            if state in dci_conf['dci'] and RV == 0:
                 for hook in dci_conf['dci'][state]:
                     dci_jobstate.create(ctx, state, 'Running %s hook' % hook,
                                         ctx.last_job_id)
